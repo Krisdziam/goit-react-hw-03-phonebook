f279226a930841ae37318b1081f1b25f07ae1b0e
Kristian <kris.dziamka@gmail.com>
2022-09-25 16:49:10 +0300

    localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -9,30 +9,27 @@ import styles from './App.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      {
-        id: 'id-1',
-        name: 'Rosie Simpson',
-        number: '459-12-56',
-      },
-      {
-        id: 'id-2',
-        name: 'Hermione Kline',
-        number: '443-89-12',
-      },
-      {
-        id: 'id-3',
-        name: 'Eden Clements',
-        number: '645-17-79',
-      },
-      {
-        id: 'id-4',
-        name: 'Annie Copeland',
-        number: '227-91-26',
-      },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem(
+        'savedContacts',
+        JSON.stringify(this.state.contacts)
+      );
+    }
+  }
+  componentDidMount() {
+    const contacts = localStorage.getItem('savedContacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({
+        contacts: parsedContacts,
+      });
+    }
+  }
 
   deleteContact = contactId => {
     const info = toast.info(`Contact deleted`, {
